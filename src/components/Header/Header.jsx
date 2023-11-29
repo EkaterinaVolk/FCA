0896469e07191da4871e3922943fbc86fa8e477e
eevitg <vketh@yandex.ru>
2023-11-29 14:36:45 +0300

    add structure

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.scss';
+
+
+export default function Header() {
+  return (
+    <div className="container__header">
+        <div className="logo">My cards</div>
+        <div className="nav">
+            <a href="#">Карточки</a>
+            <a href="#">Учить</a>
+            <a href="#">Список слов</a>
+        </div>
+    </div>
+  )
+}
