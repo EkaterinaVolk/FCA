6b9d7e6cfec9d7c51eab2af099cd243c57680c0d
eevitg <vketh@yandex.ru>
2023-12-01 10:09:37 +0300

    add styles to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,10 @@ import './Footer.scss';
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='container__footer'>
+<a className='telegram__footer' href="https://t.me/imilli"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 28 28" fill="none">
+  <path d="M17.5 11.7031L12.25 17.1719L20.125 25.375L25.375 3.5L1.75 13.0703L7 15.8047L9.625 24.0078L11.5938 21.2734" stroke="#1F1F1F" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+</svg></a>
+    </div>
   )
 }
