ca364a75b16ece77df6de97eab79317a9ab31069
eevitg <vketh@yandex.ru>
2023-12-04 17:04:27 +0300

    add some changes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,27 @@
 import './Card.scss';
+import { useState } from 'react';
 
 function Card(props) {
+
+  const [flip, setFlip] = useState(true);
+
+  const handleFlipState = () => {setFlip(!flip)} 
+
     return (
             <div className="container__card">
             <p className="tags__card">#{props.tags}</p>
+            {flip 
+            ? <div>
             <h4 className="word__card">{props.english}</h4>
-            <h4 className="translation__card">{props.translation}</h4>
             <p className="transcription__card">{props.transcription}</p>
             <div className="container__buttons">
             <button className="button__card">One more time</button>
             <button className="button__card">Know</button>
             </div>
-            <button className="button-flip">flip</button>
+              </div>
+            : <h4 className="translation__card">{props.translation}</h4>}         
+
+            <button className="button-flip" onClick={handleFlipState}>flip</button>
             </div>
     );
   }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,34 +1,17 @@
 import './Main.scss';
 import Card from '../Card/Card.jsx'
 import Table from '../Table/Table.jsx'
-import EditedTable from '../EditedTable/EditedTable.jsx'
 import Inputs from '../Inputs/Inputs.jsx'
 import wordlist from '../Card/wordlist.json';
-import { useState } from 'react';
-
 
 function Main() {
-  const [change, setChange] = useState(false);
 
-
-
-  const handleChangeState = () => {setChange(!change)} 
- 
   return (
     <div className='container__main'>
-    {wordlist.map((word) =>
-    <Card english={word.english} tags={word.tags} translation={word.translation} transcription={word.transcription}/>)}
+    {wordlist.map((word, index) =>
+    <Card key={index} {...word}/>)}
     <Inputs/>
-        <button onClick={handleChangeState}>Change</button>
-
-{/* Вот это не работает */}
-
-      {wordlist.map((word) => {change ?  <Table english={word.english} tags={word.tags} translation={word.translation} transcription={word.transcription}/> :  <EditedTable english={word.english} tags={word.tags} translation={word.translation} transcription={word.transcription}/>})}
-
-{/* А вот это работает */}
-
-   {/* { change ? <button>click</button>  : <Inputs/>} */}
-
+    {wordlist.map((word, index) =>  <Table key={index} {...word}/>)}
     </div>
   )
 }
