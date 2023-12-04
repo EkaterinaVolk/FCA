fa1df45c90948e985c2351e9bb6175c66ddf492a
eevitg <vketh@yandex.ru>
2023-12-04 14:28:08 +0300

    add some changes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,13 +4,14 @@ import Table from '../Table/Table.jsx'
 import EditedTable from '../EditedTable/EditedTable.jsx'
 import Inputs from '../Inputs/Inputs.jsx'
 import wordlist from '../Card/wordlist.json';
+import { useState } from 'react';
 
 
 function Main() {
-  let changeTrueOrFalse = true;
+  const [flag, setFlag] = useState(true);
 
-  function changeTOF() {
-    changeTrueOrFalse = false;
+  function editFlag() {
+    setFlag(!flag)
   }
 
   return (
@@ -18,10 +19,16 @@ function Main() {
     {wordlist.map((word) =>
     <Card english={word.english} tags={word.tags} translation={word.translation} transcription={word.transcription}/>)}
     <Inputs/>
-        <button onClick={changeTOF}>Change</button>
-      {wordlist.map((word) => <Table english={word.english} tags={word.tags} translation={word.translation} transcription={word.transcription}/> )}
-      
-      { changeTrueOrFalse ? <button>click</button>  : <Inputs/>}
+        <button onClick={editFlag}>Change</button>
+
+{/* Вот это не работает */}
+
+      {wordlist.map((word) => {flag ?  <Table english={word.english} tags={word.tags} translation={word.translation} transcription={word.transcription}/> :  <EditedTable english={word.english} tags={word.tags} translation={word.translation} transcription={word.transcription}/>})}
+
+{/* А вот это работает */}
+
+   {/* { flag ? <button>click</button>  : <Inputs/>} */}
+
     </div>
   )
 }
