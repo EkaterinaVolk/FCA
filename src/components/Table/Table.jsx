5160dae20907dbb161499e8b45914acee1100426
eevitg <vketh@yandex.ru>
2023-12-24 17:41:19 +0300

    add validation structure

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,26 +1,26 @@
 import './Table.scss';
-import { useState, useEffect } from 'react';
-
+import { useState, useEffect, useContext } from 'react';
+import {WordlistContext} from '../App/WordlistContext.jsx'
 
 export default function Table(props) {
-
+  const {context, setContext} = useContext(WordlistContext)
  
   const [changeState, setChangeState] = useState(false);
   const handleChangeState = () => {setChangeState(!changeState)} 
   
   const [english, setEnglish] = useState('')
-  const [translation, setTranslation] = useState('')
+  const [russian, setRussian] = useState('')
   const [transcription, setTranscription] = useState('')
   const [tags, setTags] = useState('')
   const [validateEnglish, setValidateEnglish] = useState('false')
-  const [validateTranslation, setValidateTranslation] = useState('false')
+  const [validateRussian, setValidateRussian] = useState('false')
   const [validateTranscription, setValidateTranscription] = useState('false')
   const [validateTags, setValidateTags] = useState('false')
 
   function handleValidate() {
-    if(!english && !translation && !transcription && !tags) {
+    if(!english && !russian && !transcription && !tags) {
       setValidateEnglish(true)
-      setValidateTranslation(true)
+      setValidateRussian(true)
       setValidateTranscription(true)
       setValidateTags(true)
     }
@@ -28,35 +28,60 @@ export default function Table(props) {
 
    function consoleLogAndClose(){
     handleValidate();
-    console.log(english, translation, transcription, tags);
-    props.saveEditedWord(english, translation, transcription, tags);
+    saveEditedWord(english, russian, transcription, tags);
+    console.log(english, russian, transcription, tags);
     handleChangeState();
   }
 
   useEffect(() => {
     setEnglish(props.english)
-    setTranslation(props.translation)
+    setRussian(props.russian)
     setTranscription(props.transcription)
     setTags(props.tags)
   }, [changeState])
+
+  function deleteWord(id) {
+    const newWordList = context.filter(word => word.id != id)
+    setContext(newWordList)
+  }
+
+    function saveEditedWord(english, russian, trasctiption, tags, id) {
+    const editedWordList = context.map(word => {
+      if (word.id === id) {
+        word.english = english;
+        word.transcription = trasctiption;
+        word.russian = russian;
+        word.tags = tags
+        return word
+      }
+      return word
+    })
+    setContext(editedWordList)
+  }
 
   return (
     <div className='container__table'>
         <div className="words-container__table">
         {changeState 
         ? 
-        <form className="words-container__table">
+        <div className="words-container__change">
+        <form className="words-container__inputs">
         <input type='text' required value={english} onChange={(e) => setEnglish(e.target.value)} className='cell__table-input' placeholder={props.english} name="english"></input>
-        <input type='text' required value={translation} onChange={(e) => setTranslation(e.target.value)} className='cell__table-input' placeholder={props.translation} name="translation"></input>
+        <input type='text' required value={russian} onChange={(e) => setRussian(e.target.value)} className='cell__table-input' placeholder={props.russian} name="russian"></input>
         <input type='text' required value={transcription} onChange={(e) => setTranscription(e.target.value)} className='cell__table-input' placeholder={props.transcription} name="transcription"></input>
         <input type='text' required value={tags} onChange={(e) => setTags(e.target.value)} className='cell__table-input' placeholder={props.tags} name="tags"></input>
-        {validateEnglish && <h5>нужно заполнить все поля</h5>}
                 {/* не понимаю, почему ошибку выводит сразу при нажатии кнопки Change, когда открываются инпуты */}
         </form>
-                
+        <div className="words-container__table">
+        <div className='cell__table-validation'>{validateEnglish && <span>*fill out</span>}</div>
+        <div className='cell__table-validation'>{validateRussian && <span>*fill out</span>}</div>
+        <div className='cell__table-validation'>{validateTranscription && <span>*fill out</span>}</div>
+        <div className='cell__table-validation'>{validateTags && <span>*fill out</span>}</div>
+        </div>
+       </div>
         : <div className="words-container__table">
         <div className='cell__table'>{props.english}</div>
-        <div className='cell__table'>{props.translation}</div>
+        <div className='cell__table'>{props.russian}</div>
         <div className='cell__table'>{props.transcription}</div>
         <div className='cell__table'>{props.tags}</div>
 
@@ -65,7 +90,7 @@ export default function Table(props) {
         ? <div className="container__table-buttons"><button className='save-button__table' type='button' onClick={consoleLogAndClose  }>save</button>
         <button className='cancel-button__table' onClick={handleChangeState}>cancel</button> </div>
         :        <button className='change-button__table' onClick={handleChangeState}>change</button>}
-                            <button className='delete-button__table' onClick={() =>props.deleteWord(props.id)}>del</button>
+                            <button className='delete-button__table' onClick={() =>deleteWord(props.id)}>del</button>
         </div>
     </div>
   )
