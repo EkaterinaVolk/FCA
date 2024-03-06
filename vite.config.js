59d0d6d5faa7837a09e248b6266719a1850ef21f
Ekaterina Volkova <108803039+EkaterinaVolk@users.noreply.github.com>
2024-03-06 14:49:06 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/FCA/",
   plugins: [react()],
+    server: {
+    proxy: {
+      '/api': {
+        target: 'https://itgirlschool.justmakeit.ru/',
+        changeOrigin: true,
+      },
+    },
+  },
 })
